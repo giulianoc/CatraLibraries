d1561de67fb62724767aa12bd3ce57290cf2da7d
Catrambone Giuliano <giulianocatrambone@gmail.com>
2019-06-22 17:18:50 +0200

    mysql headers centos vs ubuntu

--- DBConnectionPool/src/MySQLConnection.h
+++ DBConnectionPool/src/MySQLConnection.h
@@ -14,12 +14,12 @@
  */
 #include "catralibraries/DBConnectionPool.h"
 #include <string>
-#include "mysql-cppconn-8/jdbc/mysql_connection.h"
-#include <mysql-cppconn-8/jdbc/cppconn/driver.h>
-#include <mysql-cppconn-8/jdbc/cppconn/exception.h>
-#include <mysql-cppconn-8/jdbc/cppconn/resultset.h>
-#include <mysql-cppconn-8/jdbc/cppconn/prepared_statement.h>
-#include <mysql-cppconn-8/jdbc/cppconn/statement.h>
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/statement.h>
 
 
 class MySQLConnection : public DBConnection {
