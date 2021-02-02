3cfb1304787f1630d0233880308788568f5e64e6
giuliano <giuliano.catrambone@rsi.ch>
2021-02-02 10:39:17 +0100

    added deploy script

--- prepareToDeploy/createTarVersion.sh
+++ prepareToDeploy/createTarVersion.sh
@@ -2,12 +2,17 @@
 
 date
 
+deployDirectory=/opt/catrasoftware/deploy
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 version=$(cat ./version.txt)
 
 currentDir=$(pwd)
 moduleName=$(basename $currentDir)
 
-cd /opt/catrasoftware/deploy
+cd $deployDirectory
 tarFileName=$moduleName-$version-ubuntu.tar.gz
 
 rm -rf $moduleName-$version
@@ -18,5 +23,7 @@ rm -rf $moduleName-$version
 cd $currentDir
 
 echo ""
+printf "${RED}"
 echo "$tarFileName ready"
+printf "${NC}"
 
