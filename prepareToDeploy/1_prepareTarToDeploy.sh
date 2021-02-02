268fd522f120bbc1ddc8d88550ed0e3b3d21424b
giuliano <giuliano.catrambone@rsi.ch>
2021-02-02 09:35:41 +0100

    scripts...

--- prepareToDeploy/1_prepareTarToDeploy.sh
+++ prepareToDeploy/1_prepareTarToDeploy.sh
@@ -13,26 +13,33 @@ tagName=$1
 tagMessage=$2
 
 RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 
 echo ""
 printf "${RED}git commit -am $tagMessage\n"
+printf "${NC}"
 git commit -am "$tagMessage"
 
 echo ""
 printf "${RED}./prepareToDeploy/setTag.sh $tagName $tagMessage"
+printf "${NC}"
 ./prepareToDeploy/setTag.sh $tagName "$tagMessage"
 
 echo ""
 printf "${RED}git commit -am $tagMessage"
+printf "${NC}"
 git commit -am "$tagMessage"
 
 echo ""
 printf "${RED}git push"
+printf "${NC}"
 git push
 
 read -n 1 -s -r -p "Press any key to continue"
 
 echo ""
 printf "${RED}./prepareToDeploy/createTarVersion.sh"
+printf "${NC}"
 ./prepareToDeploy/createTarVersion.sh
 
