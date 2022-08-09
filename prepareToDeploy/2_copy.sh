793c26bf90707468649e6c4d18307e97e4977173
giulianoc <giulianocatrambone@gmail.com>
2022-08-09 13:13:19 +0200

    compiled for hetzner

--- prepareToDeploy/2_copy.sh
+++ prepareToDeploy/2_copy.sh
@@ -7,11 +7,12 @@ version=$(cat ./version.txt)
 currentDir=$(pwd)
 moduleName=$(basename $currentDir)
 
-linuxName=$(cat /etc/os-release | grep "^ID=" | cut -d'=' -f2)
-#linuxName using centos will be "centos", next remove "
-linuxName=$(echo $linuxName | awk '{ if (substr($0, 0, 1) == "\"") printf("%s", substr($0, 2, length($0) - 2)); else printf("%s", $0) }')
+#linuxName=$(cat /etc/os-release | grep "^ID=" | cut -d'=' -f2)
+##linuxName using centos will be "centos", next remove "
+#linuxName=$(echo $linuxName | awk '{ if (substr($0, 0, 1) == "\"") printf("%s", substr($0, 2, length($0) - 2)); else printf("%s", $0) }')
 
-tarFileName=$moduleName-$version-$linuxName.tar.gz
+#tarFileName=$moduleName-$version-$linuxName.tar.gz
+tarFileName=$moduleName-$version.tar.gz
 
 echo -n "deploy su mms cloud/test? " 
 read deploy
@@ -27,6 +28,22 @@ if [ "$deploy" == "y" ]; then
 
 	echo "aws-mms-t-transcoder-irl-1"
 	scp -i ~/ssh-keys/aws-mms-key-ireland.pem /opt/catrasoftware/deploy/$tarFileName mms@ec2-54-73-213-29.eu-west-1.compute.amazonaws.com:/opt/catramms
+	date
+fi
+
+echo -n "deploy su cloud production? " 
+read deploy
+if [ "$deploy" == "y" ]; then
+	echo "hetzner-api-gui-2"
+	scp -P 9255 -i ~/ssh-keys/hetzner-mms-key.pem /opt/catrasoftware/deploy/$tarFileName mms@168.119.250.162:/opt/catramms
+	date
+
+	echo "hetzner-engine-db-2"
+	scp -P 9255 -i ~/ssh-keys/hetzner-mms-key.pem /opt/catrasoftware/deploy/$tarFileName mms@168.119.60.134:/opt/catramms
+	date
+
+	echo "hetzner-transcoder-1"
+	scp -P 9255 -i ~/ssh-keys/hetzner-mms-key.pem /opt/catrasoftware/deploy/$tarFileName mms@49.12.33.127:/opt/catramms
 	date
 fi
 
